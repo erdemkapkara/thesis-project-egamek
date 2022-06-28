d7f8fd5a622d188e1a19ec21de903f74fd011c67
ErdemKapkara <erdemkapkara.uk@gmail.com>
2022-06-28 12:24:24 +0200

    Initial commit by Erdem

---
+++ Egamek/Controllers/FindGameController.cs
@@ -0,0 +1,28 @@
+﻿using Egamek_Business.Interfaces;
+using Egamek_Business.ViewModels.HomeViewModel;
+using Egamek_Core;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Egamek.Controllers
+{
+    public class FindGameController : Controller
+    {
+        private readonly IGameService _gameService;
+        private readonly IUnitOfWork _unitOfWork;
+        public FindGameController(IGameService gameService, IUnitOfWork unitOfWork)
+        {
+            _gameService = gameService;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
